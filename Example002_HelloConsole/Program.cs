c9bbba26c6dc4105a77b9cecd0ecf2d374903080
Main Villain <deserteagle081@gmail.com>
2023-08-31 22:21:08 +0300

    add example002 hello user

---
+++ Example002_HelloConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите Ваше имя");
+string myString = Console.ReadLine();
+Console.Write("Hello,");
+Console.Write(myString);
