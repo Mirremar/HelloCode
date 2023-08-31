96a7cb6fedefa56ca543b4f2b08ad249457533c7
Main Villain <deserteagle081@gmail.com>
2023-09-01 00:03:07 +0300

    added Example005

---
+++ Example005_SpecialGreet/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string Username = Console.ReadLine();
+if(Username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура,это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, " + Username);    
+}
