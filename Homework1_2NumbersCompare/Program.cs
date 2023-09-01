f43a83eae989505b968b4d548128ca8a8c6c98dc
Main Villain <deserteagle081@gmail.com>
2023-09-01 14:38:54 +0300

    added Homework1 — 2NumbersCompare

---
+++ Homework1_2NumbersCompare/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 0;
+int b = 0;
+Console.WriteLine("Введите два целых числа");
+Console.WriteLine("Введите a");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b");
+b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("max = " + a);
+    Console.WriteLine("min = " + b);
+}
+else if (a == b)
+    {
+        Console.WriteLine("max = min = " + a);
+    }
+    else
+    {
+    Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a);
+    }
