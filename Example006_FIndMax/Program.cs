80185f00fb5ec96bfde564fff9f0117c02c1236f
Main Villain <deserteagle081@gmail.com>
2023-09-01 13:03:14 +0300

    added example006

---
+++ Example006_FIndMax/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = {1,3,6,8,5};
+int i = 0;
+int max = numbers[0];
+while (i < numbers.Length) 
+{
+    if (numbers[i] > max)
+    {
+    max = numbers[i];
+    }
+    i++;
+
+}
+Console.WriteLine(max);
