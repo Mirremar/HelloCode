0f195765849bbba2eb7b5078c081af180ef6bda9
Main Villain <deserteagle081@gmail.com>
2023-09-01 15:54:33 +0300

    added Homework4

---
+++ Homework4_EvenNumbersOutput/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число, до которого будут выведены все чётные числа");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (N < 2)
+{
+    Console.WriteLine("Число должно быть больше либо равно 2");
+}
+while (count <= N)
+{
+    Console.Write(count + " ");
+    count += 2;
+}
