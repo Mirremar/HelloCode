af758b939488c9b52e188a6c452708e435d206d7
Main Villain <deserteagle081@gmail.com>
2023-09-06 22:04:18 +0300

    added examples 8,9,10,11

---
+++ Example010_FindIndex/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = {1,2,3,4,18,6,7,18};
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
